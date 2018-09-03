ec0c26f9ba19c075f0b76dfb262a93f2b1b68a36
chenshuichuan <chenshuichuan@users.noreply.github.com>
2018-09-03 16:04:15 +0800

    add task

---
+++ src/main/java/llcweb/com/dao/repository2/AttendanceRepository.java
@@ -0,0 +1,24 @@
+package llcweb.com.dao.repository2;
+
+
+import llcweb.com.domain.models.Attendance;
+import llcweb.com.domain.models.File;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by:ricardo
+ * Description: 签到类的repository类
+ * Date: 2018/8/22
+ */
+public interface AttendanceRepository extends JpaRepository<Attendance,Integer>{
+
+    /**
+     * 分页动态查询
+     **/
+    Page<File> getPage(Specification<File> specification, Pageable pageable);
+
+}

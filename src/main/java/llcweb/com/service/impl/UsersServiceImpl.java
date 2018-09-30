ff19e15ce8980f759cc7e0ca34a28a0c251e7d32
chenshuichuan <chenshuichuan@users.noreply.github.com>
2018-09-30 15:22:32 +0800

    little change

--- src/main/java/llcweb/com/service/impl/UsersServiceImpl.java
+++ src/main/java/llcweb/com/service/impl/UsersServiceImpl.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
